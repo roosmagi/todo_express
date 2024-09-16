d2c39495a48a621a623c815de8c6884c9c235152
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-09-16 09:46:48 +0300

    Read data from files

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express")
 const app = express()
+const port = 4009
+const fs = require('fs')
+
 
 const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
-    res.render("index")
+    // get data from file
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        } 
+        // tasks list data from file
+        const tasks = data.split("\n")
+        res.render('index',{tasks: tasks})
+    });
 })
 
-app.listen(3002, () =>{
-    console.log("Example app is startet at http://localhost:3002")
+app.listen(4009, () => {
+    console.log("Example app is started at http://localhost:4009")
 })
