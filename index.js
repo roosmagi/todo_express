1a4d8a9f4c0ef7c2e53a91cfe6fd09a21ed69282
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-09-23 10:31:26 +0300

    Delete single task data from file

--- index.js
+++ index.js
@@ -1,26 +1,95 @@
+const { rejects } = require("assert")
 const express = require("express")
 const app = express()
-const port = 4009
 const fs = require('fs')
-
-
 const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
-app.get("/", (req, res) => {
-    // get data from file
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return
-        } 
-        // tasks list data from file
-        const tasks = data.split("\n")
-        res.render('index',{tasks: tasks})
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            } 
+            // tasks list data from file
+            const tasks = JSON.parse(data)
+            resolve(tasks)
     });
-})
+  }) 
+}
 
-app.listen(4009, () => {
-    console.log("Example app is started at http://localhost:4009")
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) => {
+        // get data from file
+        fs.writeFile(filename, data , "utf-8", err => {
+            if (err) {
+                console.error(err);
+                return;
+            } 
+            resolve(true)
+        }) ;
+    })
+} 
+
+app.get('/', (req, res) => {
+    // tasks list data from file
+    readFile('./tasks.json') 
+        .then(tasks => {
+            console.log(tasks)
+            res.render('index', {tasks: tasks})
+    })    
+})    
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) =>{
+    // tasks list data from file
+    readFile('./tasks.json')
+        .then(tasks => {
+            // add new task
+            //create new id automatically
+            let index
+            if(tasks.length === 0 )
+           {
+            index = 0
+           } else {
+                index = tasks[tasks.length-1].id + 1;
+           } 
+           //create task object
+           const newTask = {
+                "id" : index,
+                "task" : req.body.task
+           } 
+           console.log(newTask)
+           // add form sent task to tasks array
+           tasks.push(newTask)
+           data = JSON.stringify(tasks, null, 2)
+           writeFile("tasks.json", data)
+            // redirect to / to see results
+            res.redirect('/')  
+        })
+    })
+
+app.get("/delete-task/:taskId", (req, res) => {
+    let deletedTaskId = parseInt(req.params.taskId)
+    readFile("./tasks.json")
+    .then(tasks => {
+        tasks.forEach((task, index) => {
+            if(task.id === deletedTaskId){
+                tasks.splice(index, 1)
+            } 
+        })
+        data = JSON.stringify(tasks, null, 2)
+        writeFile("tasks.json", data)
+        // redirect to / to see result
+        res.redirect("/")
+        })
+    })
+
+    
+app.listen(4031, () => {
+    console.log("Example app is started at http://localhost:4031")
 })
